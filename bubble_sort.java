6ef647710aba0919a4d5eb6489eb90b001d7953f
Ankur4600 <tinkushrivastav78@gmail.com>
2024-07-10 20:48:57 +0530

    bubble sort

---
+++ bubble_sort.java
@@ -0,0 +1,34 @@
+package sorting_techniques;
+
+import java.util.Scanner;
+//program for bubblr sort
+public class bubble_sort {
+   static void bubble(int[] arr, int n) {
+        for (int i = 0; i <n; i++) {
+            for (int j = 0; j < n - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+
+        System.out.println("After bubble sort: ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+    public static void main(String args[]) {
+        int arr[] = {13, 46, 24, 52, 20, 9};
+        int n = arr.length;
+        System.out.println("Before Using Bubble Sort: ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+        bubble(arr, n);
+    }
+    
+}
